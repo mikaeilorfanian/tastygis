d62436fd05a74809a7c16606359f82dd9a44a15b
Mikaeil Orfanian <mokt@outlook.com>
2017-07-16 16:49:07 +0200

    create provider app and db model

---
+++ mazio/provider/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    language = models.CharField(max_length=2)
+    currency = models.CharField(max_length=2)
