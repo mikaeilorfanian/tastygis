62b2aa77194693030880eb4359ff1ea649ea79d5
Mikaeil Orfanian <mokt@outlook.com>
2017-07-16 21:24:23 +0200

    add db model for service areas

--- mazio/servicearea/models.py
+++ mazio/servicearea/models.py
@@ -1,3 +1,13 @@
+from django.contrib.gis import gis_models
 from django.db import models
 
-# Create your models here.
+from provider.models import Provider
+
+
+class ServiceArea(models.Model):
+    provider = models.ForeignKey(Provider, related_name='service_areas')
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    polys = gis_models.MultiPolygonField(null=True, blank=True)
+
+    objects = gis_models.GeoManager()
