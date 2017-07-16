72294b272d73a77dc2d5f3f2fde69a8d57571386
Mikaeil Orfanian <mokt@outlook.com>
2017-07-16 19:10:29 +0200

    add urls for swagger and tastypie

--- mazio/mazio/urls.py
+++ mazio/mazio/urls.py
@@ -13,9 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from tastypie.api import Api
+
+from provider.api.resources import ProviderResource
+
+
+v1_api = Api(api_name='v1')
+v1_api.register(ProviderResource())
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(v1_api.urls)),
+    url(r'^api/doc',
+        include('tastypie_swagger.urls', namespace='mazioapi_tastypie_swagger'),
+        kwargs={
+            'tastypie_api_module': v1_api,
+            'namespace': 'mazioapi_tastypie_swagger',
+            'version': '1.0'
+        }
+    ),
 ]
